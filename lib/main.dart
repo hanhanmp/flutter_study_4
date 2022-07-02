2428b92730d426d40a698dc20996470aac0dd9e8
hanhanmp <2598294105@qq.com>
2022-07-02 09:19:02 +0800

    新增

---
+++ lib/main.dart
@@ -0,0 +1,350 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class AnimatedFlipCounter extends StatelessWidget {
+  /// The value of this counter.
+  ///
+  /// When a new value is specified, the counter will automatically animate
+  /// from its old value to the new value.
+  final num value;
+
+  /// The duration over which to animate the value of this counter.
+  final Duration duration;
+
+  /// The curve to apply when animating the value of this counter.
+  final Curve curve;
+
+  /// If non-null, the style to use for the counter text.
+  ///
+  /// Similar to the TextStyle property of Text widget, the style will
+  /// be merged with the closest enclosing [DefaultTextStyle].
+  final TextStyle? textStyle;
+
+  /// The text to display in front of the counter.
+  final String? prefix;
+
+  /// The text to display after the counter.
+  final String? suffix;
+
+  /// How many digits to display, after the decimal point.
+  ///
+  /// The actual [value] will be rounded to the nearest digit.
+  final int fractionDigits;
+
+  /// How many digits to display, before the decimal point.
+  ///
+  /// For example, `wholeDigits: 4` means it will pad `48` into `0048`.
+  /// Default value is `1`, setting it to `0` would turn `0.7` into `.7`.
+  /// If the actual [value] has more digits, this property will be ignored.
+  final int wholeDigits;
+
+  /// Insert a symbol between every 3 digits, for example: 1,000,000.
+  ///
+  /// Typical symbol is either a comma or a period, based on locale. Default
+  /// value is null, which disables this feature.
+  final String? thousandSeparator;
+
+  /// Insert a symbol between the integer part and the fractional part.
+  ///
+  /// Default value is a period. Can be changed to a comma for certain locale.
+  final String decimalSeparator;
+
+  /// How the digits should be placed. Can be used to control text alignment.
+  ///
+  /// Default value is `MainAxisAlignment.center`, which aligns the digits to
+  /// the center, similar to `TextAlign.center`. To mimic `TextAlign.start`,
+  /// set the value to `MainAxisAlignment.start`.
+  final MainAxisAlignment mainAxisAlignment;
+
+  const AnimatedFlipCounter({
+    Key? key,
+    required this.value,
+    this.duration = const Duration(milliseconds: 300),
+    this.curve = Curves.linear,
+    this.textStyle,
+    this.prefix,
+    this.suffix,
+    this.fractionDigits = 0,
+    this.wholeDigits = 1,
+    this.thousandSeparator,
+    this.decimalSeparator = '.',
+    this.mainAxisAlignment = MainAxisAlignment.center,
+  })  : assert(fractionDigits >= 0, "fractionDigits must be non-negative"),
+        assert(wholeDigits >= 0, "wholeDigits must be non-negative"),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final style = DefaultTextStyle.of(context).style.merge(textStyle);
+    // Layout number "8" (probably the widest digit) to see its size
+    final prototypeDigit = TextPainter(
+      text: TextSpan(text: "8", style: style),
+      textDirection: TextDirection.ltr,
+      textScaleFactor: MediaQuery.of(context).textScaleFactor,
+    )..layout();
+
+    // Find the text color (or red as warning). This is so we can avoid using
+    // `Opacity` and `AnimatedOpacity` widget, for better performance.
+    final Color color = style.color ?? Color(0xffff0000);
+
+    // Convert the decimal value to int. For example, if we want 2 decimal
+    // places, we will convert 5.21 into 521.
+    final int value = (this.value * math.pow(10, fractionDigits)).round();
+
+    // Split the integer value into separate digits.
+    // For example, to draw 521, we split it into [5, 52, 521].
+    List<int> digits = value == 0 ? [0] : [];
+    int v = value.abs();
+    while (v > 0) {
+      digits.add(v);
+      v = v ~/ 10;
+    }
+    while (digits.length < wholeDigits + fractionDigits) {
+      digits.add(0); // padding leading zeroes
+    }
+    digits = digits.reversed.toList(growable: false);
+
+    // Generate the widgets needed for digits before the decimal point.
+    final integerWidgets = <Widget>[];
+    for (int i = 0; i < digits.length - fractionDigits; i++) {
+      final digit = _SingleDigitFlipCounter(
+        key: ValueKey(digits.length - i),
+        value: digits[i].toDouble(),
+        duration: duration,
+        curve: curve,
+        size: prototypeDigit.size,
+        color: color,
+      );
+      integerWidgets.add(digit);
+    }
+    // Insert "thousand separator" widgets if needed.
+    if (thousandSeparator != null) {
+      int counter = 0;
+      for (int i = integerWidgets.length; i > 0; i--) {
+        if (counter > 0 && counter % 3 == 0) {
+          integerWidgets.insert(i, Text(thousandSeparator!));
+        }
+        counter++;
+      }
+    }
+
+    return DefaultTextStyle.merge(
+      style: style,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: mainAxisAlignment,
+        children: [
+          if (prefix != null) Text(prefix!),
+          ClipRect(
+            child: TweenAnimationBuilder(
+              // Animate the negative sign (-) appear and disappearing
+              duration: duration,
+              tween: Tween(end: value < 0 ? 1.0 : 0.0),
+              builder: (_, double v, __) => Center(
+                widthFactor: v,
+                child: Opacity(opacity: v, child: Text("-")),
+              ),
+            ),
+          ),
+          // Draw digits before the decimal point
+          ...integerWidgets,
+          // Draw the decimal point
+          if (fractionDigits != 0) Text(decimalSeparator),
+          // Draw digits after the decimal point
+          for (int i = digits.length - fractionDigits; i < digits.length; i++)
+            _SingleDigitFlipCounter(
+              key: ValueKey("decimal$i"),
+              value: digits[i].toDouble(),
+              duration: duration,
+              curve: curve,
+              size: prototypeDigit.size,
+              color: color,
+            ),
+          if (suffix != null) Text(suffix!),
+        ],
+      ),
+    );
+  }
+}
+
+class _SingleDigitFlipCounter extends StatelessWidget {
+  final double value;
+  final Duration duration;
+  final Curve curve;
+  final Size size;
+  final Color color;
+
+  const _SingleDigitFlipCounter({
+    Key? key,
+    required this.value,
+    required this.duration,
+    required this.curve,
+    required this.size,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder(
+      tween: Tween(end: value),
+      duration: duration,
+      curve: curve,
+      builder: (_, double value, __) {
+        final whole = value ~/ 1;
+        final decimal = value - whole;
+        final w = size.width;
+        final h = size.height;
+
+        return SizedBox(
+          width: w,
+          height: h,
+          child: Stack(
+            children: <Widget>[
+              _buildSingleDigit(
+                digit: whole % 10,
+                offset: h * decimal,
+                opacity: 1 - decimal,
+              ),
+              _buildSingleDigit(
+                digit: (whole + 1) % 10,
+                offset: h * decimal - h,
+                opacity: decimal,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildSingleDigit({
+    required int digit,
+    required double offset,
+    required double opacity,
+  }) {
+    // Try to avoid using the `Opacity` widget when possible, for performance.
+    final child;
+    if (color.opacity == 1) {
+      // If the text style does not involve transparency, we can modify
+      // the text color directly.
+      child = Text(
+        '$digit',
+        style: TextStyle(color: color.withOpacity(opacity.clamp(0, 1))),
+      );
+    } else {
+      // Otherwise, we have to use the `Opacity` widget.
+      child = Opacity(
+        opacity: opacity.clamp(0, 1),
+        child: Text('$digit'),
+      );
+    }
+    return Positioned(
+      bottom: offset,
+      child: child,
+    );
+  }
+}
+//import 'package:animated_flip_counter/animated_flip_counter.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double _value = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("计数器改进"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            AnimatedFlipCounter(
+              value: 1000 + _value,
+            ),
+            AnimatedFlipCounter(
+              value: 1000 + _value,
+              wholeDigits: 4,
+              fractionDigits: 2,
+              thousandSeparator: ',',
+            ),
+            AnimatedFlipCounter(
+              value: _value,
+              duration: Duration(seconds: 1),
+              curve: Curves.elasticOut,
+              textStyle: TextStyle(fontSize: 60, color: Colors.blue),
+            ),
+            AnimatedFlipCounter(
+              value: _value,
+              duration: Duration(seconds: 1),
+              curve: Curves.bounceOut,
+              wholeDigits: 4,
+              fractionDigits: 2,
+              textStyle: TextStyle(fontSize: 40, color: Colors.blue),
+            ),
+            AnimatedFlipCounter(
+              value: _value,
+              prefix: "Level ",
+              textStyle: TextStyle(
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -8.0,
+                color: Colors.yellow,
+                shadows: [
+                  BoxShadow(
+                    color: Colors.orange,
+                    offset: Offset(2, 4),
+                    blurRadius: 4,
+                  ),
+                ],
+              ),
+            ),
+            AnimatedFlipCounter(
+              value: _value + 0.48,
+              fractionDigits: 2,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [ 1,24].map(_buildButton).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton(num value) {
+    return Column(
+      children: [
+        ElevatedButton(
+          child: Text('+$value'),
+          onPressed: () => setState(() => _value += value),
+        ),
+        ElevatedButton(
+          child: Text('-$value'),
+          onPressed: () => setState(() => _value -= value),
+        ),
+      ],
+    );
+  }
+}
